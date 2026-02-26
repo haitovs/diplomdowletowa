2729cabc9efa3e3b7783e2a2bcd3eac8a68a9a31
haitovs <spispokistm@gmail.com>
2026-02-26 23:33:31 +0500

    corrected

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,7 @@ import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET || process.env.AUTH_SECRET,
   providers: [
     CredentialsProvider({
       name: "credentials",
